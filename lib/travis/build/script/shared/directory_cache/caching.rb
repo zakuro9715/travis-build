b645e4e73a84a3ef9eb2136a53035be68ca5f78e
Hiro Asari <asari.ruby@gmail.com>
2015-08-07 13:25:41 -0400

    Add barebones structure for Cashing backend

--- lib/travis/build/script/shared/directory_cache/caching.rb
+++ lib/travis/build/script/shared/directory_cache/caching.rb
@@ -1,10 +1,60 @@
 require 'shellwords'
+require 'travis/build/script/shared/directory_cache/casher'
 
 module Travis
   module Build
     class Script
       module DirectoryCache
         class Caching
+          include Casher
+          require 'jwt'
+
+          attr_reader :sh, :data, :slug, :start, :msgs
+
+          def initialize(sh, data, slug, start = Time.now)
+            @sh = sh
+            @data = data
+            @slug = slug
+            @start = start
+            @msgs = []
+          end
+
+          def setup
+            fold 'Setting up build cache' do
+              install
+              fetch
+              add(directories) if data.cache?(:directories)
+            end
+          end
+
+          def fetch
+            run('fetch', url, timing: true)
+          end
+
+          def add(*paths)
+          end
+
+          private
+          def url
+            payload = <<-EOF
+            {
+              "iss": #{ENV['JWT_ISSUER']},
+              "jti": "",
+              "iat": #{Time.now.to_i},
+              "exp": #{Time.now.to_i + 300},
+              "payload": {
+                "repo_slug": #{data.slug},
+                "repo_id": #{data.github_id},
+                "branch": data.branch,
+                "backend": "s3",
+                "cache_slug": #{slug}
+              }
+            }
+            EOF
+
+            token = JWT.encode(payload, ENV['JWT_SECRET'], 'HS256')
+            "https://caching-staging.travis-ci.org/cache?token=" << token
+          end
 
         end
       end
--- spec/build/script/directory_cache/caching_spec.rb
+++ spec/build/script/directory_cache/caching_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+describe Travis::Build::Script::DirectoryCache::Caching, :sexp do
+  let(:s3_options)    { { bucket: 's3_bucket', secret_access_key: 's3_secret_access_key', access_key_id: 's3_access_key_id' } }
+  let(:cache_options) { { fetch_timeout: 20, push_timeout: 30, type: 's3', s3: s3_options } }
+  let(:data)          { PAYLOADS[:push].deep_merge(config: config, cache_options: cache_options, job: { branch: branch, pull_request: pull_request }) }
+  let(:config)        { {} }
+  let(:pull_request)  { nil }
+  let(:branch)        { 'master' }
+  let(:sh)            { Travis::Shell::Builder.new }
+  let(:cache)         { described_class.new(sh, Travis::Build::Data.new(data), 'ex a/mple', Time.at(10)) }
+  let(:subject)       { sh.to_sexp }
+
+  describe 'setup' do
+    before do
+      cache.setup
+    end
+    it { should include_sexp :cmd }
+  end
+end
